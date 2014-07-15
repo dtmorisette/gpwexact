55b9222a157f0ab60e1f2c41f78290d6957cf811
Dallas Morisette <morisett@purdue.edu>
2014-07-15 12:47:03 -0400

    Initial commit of .gitignore

---
+++ xcode/Gpw/Gpw/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Gpw
+//
+//  Created by Dallas Morisette on 7/15/14.
+//  Copyright (c) 2014 Dallas Morisette. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
